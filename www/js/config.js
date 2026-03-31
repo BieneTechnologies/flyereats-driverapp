1891cc931ad51985a1c704401fecbb0487b06e61
BieneTechnologies <54696733+BieneTechnologies@users.noreply.github.com>
2026-03-31 21:04:02 +0530

    Add files via upload

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={
+	'ApiUrl':"https://flyereats.in/driver/api",
+	'DialogDefaultTitle':"DriverApp",
+	'mapboxToken' : 'pk.eyJ1IjoiZmx5ZXJlYXRzIiwiYSI6ImNqcW43Z3c0MzFpaDY0M20wa3AydG5vNzMifQ.U76SWZXSn-VaF-iFXFtwKw',
+	'APIHasKey':"flyereats",
+	'debug': false
+};
